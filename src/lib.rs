06726a642a6e6f046ee0ce163125918974f95578
Eugene Chou <euchou@ucsc.edu>
2023-05-28 00:44:49 -0700

    add `StdObjectStore` under `std` feature gate

--- src/lib.rs
+++ src/lib.rs
@@ -45,3 +45,53 @@ pub enum StorageAccess {
     Read,
     Write,
 }
+
+#[cfg(feature = "std")]
+pub mod standard {
+    use crate::{PersistentStorage, StorageAccess};
+    use embedded_io::adapters::FromStd;
+    use path_macro::path;
+    use std::{
+        fs::{self, File},
+        io,
+        path::{Path, PathBuf},
+    };
+
+    pub struct StdObjectStore {
+        root: PathBuf,
+    }
+
+    impl StdObjectStore {
+        pub fn new<P: AsRef<Path>>(root: P) -> io::Result<Self> {
+            fs::create_dir_all(&root)?;
+            Ok(Self {
+                root: root.as_ref().into(),
+            })
+        }
+
+        fn object_path(&self, objid: u64) -> PathBuf {
+            path![self.root / format!("{objid}")]
+        }
+    }
+
+    impl PersistentStorage for StdObjectStore {
+        type Id = u64;
+        type Io<'a> = FromStd<File>;
+        type Error = io::Error;
+
+        fn open<'a>(
+            &'a mut self,
+            objid: Self::Id,
+            access: StorageAccess,
+        ) -> Result<Self::Io<'_>, Self::Error> {
+            Ok(FromStd::new(match access {
+                StorageAccess::Read => File::options().read(true).open(self.object_path(objid)),
+                StorageAccess::Write => File::options()
+                    .read(true)
+                    .write(true)
+                    .create(true)
+                    .open(self.object_path(objid)),
+            }?))
+        }
+    }
+}
